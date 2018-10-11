8cfcafde08191d2f64e0ea8ae256cba38d1bbf3c
Grigorii Marakov <marakogr@mail.ru>
2018-10-11 17:44:13 +0300

    user profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-
   root  'static_pages#home'
   match '/signup',  to: 'users#new', via: 'get'
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts
+  resources :users
 end
