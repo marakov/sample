d44b081f48ff38190869ab019d1079e8f6fcacc4
Grigorii Marakov <marakogr@mail.ru>
2018-10-15 12:11:49 +0300

    finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   root  'static_pages#home'
-  match '/signup',  to: 'users#new', via: 'get'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :posts
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+
   match '/help', to: 'static_pages#help', via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts
-  resources :users
-end
+ end
