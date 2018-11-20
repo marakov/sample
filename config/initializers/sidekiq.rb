c797d38c63403e8ff07d84ce2272c85917203039
Grigorii Marakov <marakogr@mail.ru>
2018-11-20 17:14:13 +0300

    stil redis..

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = {url: 'redis://localhost:6379/sidekiq_test'}
+  config.redis = { :url => ENV["REDISTOGO_URL"] }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {url: 'redis://localhost:6379/sidekiq_test'}
+  config.redis = { :url => ENV["REDISTOGO_URL"] }
 end
